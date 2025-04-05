f3664b19076a9991175148725f25f6a211904b52
kissmarkr <naterivas12@gmail.com>
2025-04-05 14:20:48 -0500

    add CORS

--- src/main/java/com/example/weblibro/config/WebConfig.java
+++ src/main/java/com/example/weblibro/config/WebConfig.java
@@ -12,11 +12,7 @@ public class WebConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:4200",
-                    "https://weblibro.vercel.app",
-                    "https://*.vercel.app"
-                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
