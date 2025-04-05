368761febf4ac834790d2b12407067e736f563da
kissmarkr <naterivas12@gmail.com>
2025-04-05 13:54:20 -0500

    add CORS

--- src/main/java/com/example/weblibro/config/WebConfig.java
+++ src/main/java/com/example/weblibro/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
